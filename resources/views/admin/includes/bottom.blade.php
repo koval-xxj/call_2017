28951bf1d1722e5f4179fdfee5783b2c018c07ea
Sergey Kovalenko <koval.xxj@gmail.com>
2017-11-30 16:28:57 +0200

    added admin's templates

---
+++ resources/views/admin/includes/bottom.blade.php
@@ -0,0 +1,26 @@
+    </div>
+  </div>
+  <hr>
+  <footer>
+      <p>&copy; Vincent Gabriel 2013</p>
+  </footer>
+</div> 
+
+<!--/.fluid-container-->
+        
+        <!--vendors/jquery-1.9.1.js-->
+        <script src="/js/home/vendor/jquery-1.10.2.min.js"></script>
+        <script src="/js/home/bootstrap.min.js"></script>
+        <script src="/js/admin/libs/jquery.dataTables.min.js"></script>
+
+
+        <script src="/js/admin/scripts.js"></script>
+        <script src="/js/admin/DT_bootstrap.js"></script>
+        <script>
+        $(function() {
+            
+        });
+        </script>
+    </body>
+
+</html>
---
+++ resources/views/admin/includes/header.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    
+  <head>
+      <title>Tables</title>
+      <!-- Bootstrap -->
+      <link href="/css/admin/bootstrap.min.css" rel="stylesheet" media="screen">
+      <link href="/css/admin/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+      <link href="/css/admin/styles.css" rel="stylesheet" media="screen">
+      <link href="/css/admin/DT_bootstrap.css" rel="stylesheet" media="screen">
+      <!--[if lte IE 8]><script language="javascript" type="text/javascript" src="vendors/flot/excanvas.min.js"></script><![endif]-->
+      <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+      <!--[if lt IE 9]>
+          <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+      <![endif]-->
+      <script src="/js/admin/libs/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+  </head>
+
+  <body>
+    
+  @include('admin/includes/head_navigation')
+  
+  <div class="container-fluid">
+    <div class="row-fluid">
+      @include('admin/includes/left_menu')
+        <!--/span-->
+        <div class="span9" id="content">
