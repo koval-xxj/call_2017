3b1f684c54aed87eb4c077b2d0f01b10882f473b
self-X <daniilvl861@gmail.com>
2017-12-01 23:26:58 +0200

    Add method for show user table

--- app/Http/Controllers/Admin/BaseController.php
+++ app/Http/Controllers/Admin/BaseController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\DB;
 
 /**
  * Description of BaseController
@@ -14,12 +15,21 @@ class BaseController extends AdminController {
   public function show() {
 //    dd(Session::has('admin_login'));
 //    dd(Session::get('admin_login'));
+
     if (!$this->check_login()) {
       return $this->login_redirect();
     }
-    
-    
+
     return view('admin/base');
   }
+
+  protected function getUser()
+  {
+      $visitors = DB::table('visitors')->get();
+      return view('admin.includes.usertable', compact('visitors'));
+
+  }
+
+
   
 }
